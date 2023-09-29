aede565e5858bbcdd83d99be51f21be2eb0d6a12
SilverMight <joeyzee1234@gmail.com>
2023-09-28 23:55:14 -0400

    pull in CSV into vector line-by-line

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
 #include <InfluxDB.h>
 #include <InfluxDBFactory.h>
-#include <cstdlib>
 
+constexpr char delimiter = ',';
 
-int main() {
+std::vector<std::vector<double>> readCSV(std::ifstream& csvFile) {
+    std::vector<std::vector<double>> lines{};
+    std::string currentLine;
+
+    while(std::getline(csvFile, currentLine)) {
+        //std::cout << currentLine;
+
+        std::vector<double> currentLineVec;
+        std::stringstream currentLineSS (currentLine);
+        
+        std::string curNumStr;
+        while(std::getline(currentLineSS, curNumStr, delimiter)) {
+            currentLineVec.push_back(std::stod(curNumStr));
+            //std::cout << curNumStr << " ";
+        }
+        lines.push_back(currentLineVec);
+        //std::cout << '\n';
+
+    }
+
+    return lines;
+    
+}
+
+int main(int argc, char * argv[]) {
     // Only continue running if INFLUX_HOST_URL can be assigned successfully; 
     const char * hostUrl;
     if(!(hostUrl = std::getenv("INFLUX_HOST_URL"))) {
@@ -18,6 +48,17 @@ int main() {
 
     for(auto query : db->query("SHOW DATABASES")) {
         std::cout << query.getTags() << '\n';
+    }
+
+    std::ifstream argFile {argv[1]};
+
+    std::vector<std::vector<double>> csvData {readCSV(argFile)};
+
+    for(auto line : csvData) {
+        for(auto num: line) {
+            std::cout << num << " , ";
+        }
+        std::cout << '\n';
     }
 
     return 0;
