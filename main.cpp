dd6060fc86d515aef9b89e42d35608b2c87b40b5
SilverMight <joeyzee1234@gmail.com>
2023-09-30 00:45:00 -0400

    Batch writing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
+#include <chrono>
 #include <iostream>
 #include <cstdlib>
+#include <ratio>
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <thread>
 
 #include <InfluxDB.h>
 #include <InfluxDBFactory.h>
@@ -43,6 +46,7 @@ int main(int argc, char * argv[]) {
     }
 
     // connect
+    std::cout << hostUrl << "\n";
     auto db = influxdb::InfluxDBFactory::Get(hostUrl);
     db->createDatabaseIfNotExists();
 
@@ -54,11 +58,31 @@ int main(int argc, char * argv[]) {
 
     std::vector<std::vector<double>> csvData {readCSV(argFile)};
 
-    for(auto line : csvData) {
-        for(auto num: line) {
-            std::cout << num << " , ";
-        }
-        std::cout << '\n';
+    /* 
+    db->batchOf(10000);
+    int count = 0;
+    for(const auto& line : csvData) {
+        //influxdb::Point curPoint{"something"};
+        //curPoint.addField("Field 1", line[1]).addField("Field 2", line[2]);
+        std::cout << line[0] << " " << line[1] << " " << line[2] << '\n';
+
+        //std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds> now = std::chrono::system_clock::now();
+        std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds> now{std::chrono::nanoseconds(static_cast<int>(line[0]))};
+        //const auto curTime = now + std::chrono::nanoseconds((int)line[0]);
+        
+        db->write(influxdb::Point{"Something"}.addField("Field 1", line[1]).addField("Field 2", line[2]).setTimestamp(now));
+        count++;
+        if(count == 10000 - 1) {
+            db->flushBatch();
+            count = 0;
+        }         
+        std::this_thread::sleep_for(std::chrono::nanoseconds(100));
+    }
+    */
+
+    std::vector<influxdb::Point> points = db->query("SELECT * FROM Something");
+    for(const auto& point : points) {
+        std::cout << point.getFields() << '\n';
     }
 
     return 0;
